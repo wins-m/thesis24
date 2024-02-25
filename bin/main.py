9a2643cf88503377439e7afd0cda6a00b4af4ff7
swmao <WinsM@pku.edu.cn>
2024-02-25 15:09:10 +0800

    [A] main steps

---
+++ bin/main.py
@@ -0,0 +1,17 @@
+"""
+Run the whole project and report tables & graphs.
+
+Steps:
+------
+
+1. token preparation: (period, fundcode)
+    module `data_process`
+2. model embeddings: (period, stockcode) CRUTIAL!
+3. similarity based on embeddings; other baselines
+4. data description for 1~3
+5. evaluation: return, volatility
+
+"""
+
+if __name__ == '__main__':
+    pass
